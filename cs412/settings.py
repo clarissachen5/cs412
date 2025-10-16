04b84302d0865353cc79eb4f575e2de7733ef8b6
clarissachen5 <clarissachen5@gmail.com>
2025-10-16 11:30:25 -0400

    changed media root oder of operations

--- cs412/settings.py
+++ cs412/settings.py
@@ -136,7 +136,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
 MEDIA_URL = "media/"  # note: no leading slash!
 
 import socket
@@ -146,3 +146,5 @@ CS_DEPLOYMENT_HOSTNAME = "cs-webapps.bu.edu"
 if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
     STATIC_URL = "/clchen5/static/"
     MEDIA_URL = "/clchen5/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")

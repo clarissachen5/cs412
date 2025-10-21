173ad8816ae8e035c413f7a04d6559d3bdb49a35
clarissachen5 <clarissachen5@gmail.com>
2025-10-21 12:08:14 -0400

    finished assignment 7 prep videos

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User  # for authentification
 
 
 # Create your models here.
@@ -12,6 +13,7 @@ class Article(models.Model):
     published = models.DateTimeField(auto_now=True)
     # image_url = models.URLField(blank=True)
     image_file = models.ImageField(blank=True)  # an actual image
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """return a string representation of this model instance."""

316f78b9ce764bd2682a97eda35fb8ea97b26c3a
clarissachen5 <clarissachen5@gmail.com>
2025-10-21 22:21:07 -0400

    successfully finished step 1 of task 1

--- mini_insta/models.py
+++ mini_insta/models.py
@@ -4,6 +4,7 @@
 
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -15,6 +16,7 @@ class Profile(models.Model):
     profile_image_url = models.TextField(blank=True)
     bio_text = models.TextField(blank=True)
     join_date = models.TextField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a string representation of this model instance."""
